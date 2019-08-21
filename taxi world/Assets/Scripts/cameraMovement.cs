94b3dc65f220af93a6fb6b20361af2489ba5dfeb
Jakub <kubawitt321@gmail.com>
2019-08-21 23:19:22 +0100

    1

---
+++ taxi world/Assets/Scripts/cameraMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMovement : MonoBehaviour
+{
+    Vector3 touchStart;
+    Vector3 mousePos;
+    public float sensivity;
+
+    void Update()
+    {
+
+        mousePos = Input.mousePosition;
+        mousePos.z = 1;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            touchStart = Camera.main.ScreenToWorldPoint(mousePos);
+        }
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 direction = touchStart - Camera.main.ScreenToWorldPoint(mousePos);
+            Camera.main.transform.localPosition += new Vector3(direction.x, 0, direction.z);
+        }
+
+
+
+
+
+    }
+}
