d1164bb2c9e5e0f0205b99ca28125e65f921e4c5
Jakub <kubawitt321@gmail.com>
2019-08-25 22:40:35 +0200

    1

---
+++ Siberia escape/Assets/Scripts/playeController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playeController : MonoBehaviour
+{
+   public float speed;
+   public float sensitivity;
+    Rigidbody rb;
+
+    float rotX = 0F;
+    float rotY = 0f;
+
+
+    private void Start()
+    {
+       
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    void movement()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+
+        transform.position += Camera.main.transform.TransformDirection(new Vector3(horizontalInput, 0, verticalInput)) * Time.deltaTime * speed;
+
+       
+
+      
+    }
+
+    void mouseLook()
+    {
+        rotX += Input.GetAxis("Mouse X") * sensitivity;
+        rotY += Input.GetAxis("Mouse Y") * sensitivity;
+        rotY = Mathf.Clamp(rotY, -80, 80);
+        Camera.main.transform.localEulerAngles = new Vector3(-rotY, rotX,0);
+    }
+    void Update()
+    {
+        movement();
+        mouseLook();
+
+
+    }
+
+}
